dc0e4d8a612d0a8cfcc79315883c3648968154a6
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-11 20:20:27 +0300

    added auth to websocket connection

--- src/Gateways/chat.gateway.ts
+++ src/Gateways/chat.gateway.ts
@@ -1,23 +1,31 @@
 import {
   MessageBody,
-  SubscribeMessage,
-  WebSocketGateway,
+  ConnectedSocket,
   WebSocketServer,
+  WebSocketGateway,
+  SubscribeMessage,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { UseGuards } from '@nestjs/common';
+import { WebSocketGuard } from 'src/Guards/websocket.guard';
 
 @WebSocketGateway({
   cors: {
-    origin: 'http://127.0.0.1:5500',
+    origin: ['http://127.0.0.1:5500', 'http://localhost:3000'],
     credentials: true,
   },
   allowEIO3: true,
 })
 export class ChatGateway {
   @WebSocketServer()
-  server;
+  server: Server;
 
+  @UseGuards(WebSocketGuard)
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string): void {
+  handleMessage(
+    @MessageBody() message: string,
+    @ConnectedSocket() socket: Socket,
+  ): void {
     this.server.emit('message', message);
   }
 }
