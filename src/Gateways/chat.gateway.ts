105ace7850c78d255c2d716773f42b52252b8da6
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-31 20:33:30 +0300

    users can send private messages to each other now

--- src/Gateways/chat.gateway.ts
+++ src/Gateways/chat.gateway.ts
@@ -5,7 +5,7 @@ import {
   WebSocketGateway,
   SubscribeMessage,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { UseGuards } from '@nestjs/common';
 import { WebSocketGuard } from 'src/Guards/websocket.guard';
 import { WebSocketService } from 'src/Guards/websocket.service';
@@ -19,27 +19,40 @@ import { WebSocketService } from 'src/Guards/websocket.service';
 })
 export class ChatGateway {
   constructor(private webSocket: WebSocketService) {}
-
   @WebSocketServer()
-  server;
+  server: Server;
+
+  @UseGuards(WebSocketGuard)
+  @SubscribeMessage('connection')
+  async handleConnection(@ConnectedSocket() socket: Socket): Promise<void> {
+    const sender = await this.webSocket.findUserFromSocket(socket);
+    const connectedSockets = await this.server.fetchSockets();
+    const connectedUsers = async () =>
+      await Promise.all(
+        connectedSockets.map(async (socket) => await this.webSocket.findUserFromSocket(socket)),
+      ).then((res) => res.filter((user) => user['name'] !== sender['name']));
+
+    socket.emit('users', await connectedUsers());
+    socket.broadcast.emit('user_connected', sender);
+  }
+
+  async handleDisconnect(@ConnectedSocket() socket: Socket): Promise<void> {
+    const sender = await this.webSocket.findUserFromSocket(socket);
+
+    socket.broadcast.emit('user_disconnected', sender);
+  }
 
   @UseGuards(WebSocketGuard)
   @SubscribeMessage('message')
   async handleMessage(
-    @MessageBody() message: string,
+    @MessageBody('message') message: object,
+    @MessageBody('to') to: object,
     @ConnectedSocket() socket: Socket,
   ): Promise<void> {
-    const sender = await this.webSocket.findUserFromToken(
-      socket.handshake.auth.token,
-    );
-    const receiver = await this.webSocket.findMessageReceiver(
-      socket.handshake.query.to,
-    );
+    const sender = await this.webSocket.findUserFromSocket(socket);
+    const newPayload = { message, from: sender, to };
 
-    this.server.emit('message', {
-      message,
-      from: sender,
-      to: receiver,
-    });
+    socket.to(to['id']).emit('message', newPayload);
+    socket.emit('message', newPayload);
   }
 }
