1ba202bc0ec3bab41c1d11ed74d462f0faa543a6
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-08-08 03:01:29 +0300

    initial commit

---
+++ src/Modules/cloudinary/cloudinary.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import * as streamifier from 'streamifier';
+
+@Injectable()
+export class CloudinaryService {
+  uploadFile(file: Express.Multer.File): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream((error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
+  }
+}
